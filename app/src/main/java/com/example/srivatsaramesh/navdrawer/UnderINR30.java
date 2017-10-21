e44fbf935f4fc5ba182b10950ccef5272c671549
Srivatsa Ramesh <srivatsa224@gmail.com>
2017-10-21 18:07:01 +0530

    Commit 1

---
+++ app/src/main/java/com/example/srivatsaramesh/navdrawer/UnderINR30.java
@@ -0,0 +1,13 @@
+package com.example.srivatsaramesh.navdrawer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UnderINR30 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_under_inr30);
+    }
+}
