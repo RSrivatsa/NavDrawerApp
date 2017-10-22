3194ae36e4e4fb1d9c3b42285d72605637a88521
Srivatsa Ramesh <srivatsa224@gmail.com>
2017-10-23 00:40:40 +0530

    Added Rating bar, Buy Now Button and details of the item.

--- app/src/main/java/com/example/srivatsaramesh/navdrawer/K6Specs.java
+++ app/src/main/java/com/example/srivatsaramesh/navdrawer/K6Specs.java
@@ -2,15 +2,20 @@ package com.example.srivatsaramesh.navdrawer;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class K6Specs extends AppCompatActivity {
     TextView textView;
     Button b;
+    Button rb;
+    RatingBar r;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +26,21 @@ public class K6Specs extends AppCompatActivity {
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Uri uri = Uri.parse("http://fkrt.it/hADQnTuuuN"); // missing 'http://' will cause crashed
+                Uri uri = Uri.parse("http://fkrt.it/hADQnTuuuN");
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
 
             }
 
+        });
+        r=(RatingBar)findViewById(R.id.ratingBar);
+        rb=(Button)findViewById(R.id.button2);
+        rb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              Snackbar snackbar = Snackbar.make(view,"You Rated it " + r.getRating()+" ", Toast.LENGTH_SHORT);
+                snackbar.show();
+            }
         });
     }
 }
