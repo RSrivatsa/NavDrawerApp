1d4c84aba749afe42157f739293b90ed9ec24d92
Srivatsa Ramesh <srivatsa224@gmail.com>
2017-10-22 20:50:24 +0530

    Commit 2, Added SplashScreen and few minor changes in Nav Drawer.

--- app/src/main/java/com/example/srivatsaramesh/navdrawer/UnderINR10.java
+++ app/src/main/java/com/example/srivatsaramesh/navdrawer/UnderINR10.java
@@ -2,12 +2,17 @@ package com.example.srivatsaramesh.navdrawer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class UnderINR10 extends AppCompatActivity {
-
+ TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_under_inr10);
+        t=(TextView)findViewById(R.id.textView3);
+
+
     }
 }
